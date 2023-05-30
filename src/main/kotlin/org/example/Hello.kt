afa7d3e4f00407633522f843b708af5cbccceb18
gaurav <gmadkaikar@iitkgp.ac.in>
2023-05-30 14:48:16 +0530

    swagger-core annotations added!

--- src/main/kotlin/org/example/Hello.kt
+++ src/main/kotlin/org/example/Hello.kt
@@ -20,6 +20,21 @@
 package org.example
 
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.parameters.RequestBody
+import io.swagger.v3.oas.annotations.servers.Server
+import io.swagger.v3.oas.annotations.tags.Tag
+//io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tags
 import io.vertx.core.Vertx
 import io.vertx.core.json.jackson.DatabindCodec
 import io.vertx.ext.web.Router
@@ -29,6 +44,12 @@ import io.vertx.ext.web.handler.LoggerHandler
 import io.vertx.ext.web.handler.ResponseTimeHandler
 import io.vertx.ext.web.handler.TimeoutHandler
 import io.vertx.kotlin.ext.web.api.contract.routerFactoryOptionsOf
+import javax.ws.rs.Consumes
+import javax.ws.rs.DELETE
+import javax.ws.rs.GET
+import javax.ws.rs.POST
+import javax.ws.rs.PUT
+import javax.ws.rs.Path
 
 fun getStatusCode(status: Int): Pair<Int, String>{
     val code = when (status) {
@@ -51,10 +72,28 @@ fun getStatusCode(status: Int): Pair<Int, String>{
     return retStatus
 }
 
-data class Book(val name: String, val author: String, val content: String = "", val numPages: Int = 0)
+//@Schema(name = "Book", description = "Book Class")
+class Book(val name: String, val author: String, val content: String = "", val numPages: Int = 0)
 
 // Database Layer
 // status: 0 - "200", 1 - "201", 2 - "304", 3 - "400", 4 - "404", 5 - "500"
+@OpenAPIDefinition(
+    info = Info(
+        title = "Sample book management API",
+        version = "1.0",
+        description = "Sample OpenAPI Specification for a Book Management System",
+        contact = Contact(url = "http://github.com/gaurav-madkaikar", name = "Gaurav", email = "gmadkaikar@iitkgp.ac.in")
+    ),
+    tags = [
+        Tag(name = "Book Management", description = "Managing book-related operations")
+    ],
+    servers = [
+        Server(
+            description = "Main Server",
+            url = "http://localhost:9000/"
+        )
+    ]
+)
 class Library() {
     private val myLibrary = mutableMapOf<String, Book>()
     private var status = 0
@@ -105,7 +144,7 @@ interface BookServerAPI {
     fun handleUpdate(rtContext: RoutingContext)
     fun handleDelete(rtContext: RoutingContext)
 }
-
+@Path("/")
 class BookService(private val db: Library) : BookServerAPI {
     private var status = 0
     /**
@@ -135,6 +174,38 @@ class BookService(private val db: Library) : BookServerAPI {
      *      }
      * }
      */
+    @POST
+    @Path("/books")
+    @Operation(summary = "Add a new book",
+        description = "Post book details onto the server",
+        tags = ["Book Management"],
+        requestBody = RequestBody(content = [
+                Content(mediaType = "application/json",
+                    schema = Schema(implementation = Book::class))
+            ],
+            required = true
+        ),
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Book details updated successfully!",
+                content = [
+                    Content(mediaType = "application/json",
+                        array = ArraySchema(schema = Schema(implementation = Book::class))
+                    )
+                ]
+            ),
+            ApiResponse(
+                responseCode = "201",
+                description = "New book added successfully!",
+                content = [
+                    Content(mediaType = "application/json",
+                        array = ArraySchema(schema = Schema(implementation = Book::class))
+                    )
+                ]
+            )
+        ]
+    )
     override fun handleInsert(rtContext: RoutingContext) {
         // Insert the book
         val user = rtContext.body().asPojo(Book::class.java)
@@ -170,6 +241,26 @@ class BookService(private val db: Library) : BookServerAPI {
      *      }
      * }
      */
+    @GET
+    @Path("/books/{bookName}")
+    @Operation(summary = "List the book pointed to by bookName",
+        description = "Returns a Book object",
+        tags = ["Book Management"],
+        parameters = [
+                  Parameter(`in`= ParameterIn.PATH, name = "bookName", required = true, schema = Schema(type = "string"))
+             ],
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "A particular category",
+                content = [
+                    Content(mediaType = "application/json",
+                          array = ArraySchema(schema = Schema(implementation = Book::class))
+                    )
+                ]
+            )
+        ]
+    )
     override fun handleGetBook(rtContext: RoutingContext) {
         // Get details corresponding to the user
         val name = rtContext.pathParam("bookName")
@@ -207,6 +298,23 @@ class BookService(private val db: Library) : BookServerAPI {
      *      }
      * }
      */
+    @GET
+    @Path("/books")
+    @Operation(summary = "List all the available books",
+               description = "Returns the list of all book objects",
+               tags = ["Book Management"],
+               responses = [
+                   ApiResponse(
+                       responseCode = "200",
+                       description = "List of books returned successfully!",
+                       content = [
+                           Content(mediaType = "application/json",
+                                   array = ArraySchema(schema = Schema(implementation = Book::class))
+                           )
+                       ]
+                       )
+                 ]
+    )
     override fun getListofBooks(rtContext: RoutingContext) {
         // Get list of all available users
         val bookList = db.getListofBooks()
@@ -244,6 +352,29 @@ class BookService(private val db: Library) : BookServerAPI {
      *      }
      * }
      */
+    @PUT
+    @Consumes("application/json", "application/text")
+    @Path("/books/{bookName}")
+    @Operation(summary = "Update a book",
+        description = "Update book details onto the server",
+        tags = ["Book Management"],
+        requestBody = RequestBody(content = [
+                Content(schema = Schema(implementation = Book::class))
+            ],
+            required = true
+        ),
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "List of books returned successfully!",
+                content = [
+                    Content(mediaType = "application/json",
+                        array = ArraySchema(schema = Schema(implementation = Book::class))
+                    )
+                ]
+            )
+        ]
+    )
     override fun handleUpdate(rtContext: RoutingContext) {
         // Update the song
         val book = rtContext.body().asPojo(Book::class.java)
@@ -272,6 +403,26 @@ class BookService(private val db: Library) : BookServerAPI {
      *      }
      * }
      */
+    @DELETE
+    @Path("/books/{bookName}")
+    @Operation(
+        summary = "Delete a book",
+        description = "Delete book details from the server!",
+        tags = ["Book Management"],
+        parameters = [
+            Parameter(`in`= ParameterIn.PATH, name = "bookName", required = true, schema = Schema(type = "string"))
+        ],
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Book details deleted successfully!"
+            ),
+            ApiResponse(
+                responseCode = "400",
+                description = "Book couldn't be located on the server!"
+            )
+        ]
+    )
     override fun handleDelete(rtContext: RoutingContext) {
         val name = rtContext.pathParam("bookName")
         db.deleteBook(name)
